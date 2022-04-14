3f88716087502175788b3ebbd2a43bc0d00777ac
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-14 16:49:38 +0100

    initialising the Big Boss

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.1",
+  networks:{
+    rinkeby: {
+      url: 'kGHTE7D_rMYCFeb5p1wJgPwYpZyBodjr',
+      accounts: ['a1d9e37bbf1bb27a28a029c172388c944f84bf4129cf5c803c8d09d82d6c0ddb'],
+    },
+  },
 };
