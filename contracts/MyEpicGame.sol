4e2c93e0cce7fe4e394bf8a4834d82ce6ec7eb00
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-14 14:14:03 +0100

    assigning data to minted NFT

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -3,11 +3,20 @@
 
 pragma solidity ^0.8.0;
 
+// NFT contract to inherit from.
+// OpenZeppelin implements the NFT standard with own logic
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
 // Helps with debugging smart contracts
 import "hardhat/console.sol";
 
 // Using constructor to print this statement on intialising contract
-contract MyEpicGame {
+contract MyEpicGame is ERC721 {
 
     struct CharacterAttributes {
         uint characterIndex;
@@ -17,8 +26,19 @@ contract MyEpicGame {
         uint maxHp;
         uint attackDamage;
     }
-    // arr to hold default data for characters
+// unique NFT identifier
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+// arr to hold default data for characters
 CharacterAttributes[] defaultCharacters;
+
+// map the nft's tokenId : nft attributes
+mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+//map the nft to an address: nft tokenID, can track and manage owner of nft
+mapping(address => uint256) public nftHolders;
+
 
 //  constructor RUNS ONCE holds data for intialising game state to pass into run.js
 constructor(
@@ -28,6 +48,8 @@ constructor(
        uint[] memory characterAttackDmg
      
     )
+
+    ERC721("Badasses", "Badass")
     {
         for (uint i = 0; i < characterNames.length; i += 1){
             defaultCharacters.push(CharacterAttributes({
@@ -39,8 +61,37 @@ constructor(
                 attackDamage: characterAttackDmg[i]
             }));
             CharacterAttributes memory c = defaultCharacters[i];
+            
+            // Hardhat allows 4 params to be used on console.log: uint, string, bool, address
             console.log("Done intiliasing %s w/HP %s, img %s", c.name, c.hp, c.imageURI);
         }
+        // incrementing the nfts
+        _tokenIds.increment();
+    }
+    function mintCharacterNFT(uint _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+
+        // assigns the tokenId to callers wallet using solidity variable that gives access 
+        // to person calling the contract
+        _safeMint(msg.sender, newItemId);
+
+nftHolderAttributes[newItemId] = CharacterAttributes({
+    characterIndex: _characterIndex,
+      name: defaultCharacters[_characterIndex].name,
+      imageURI: defaultCharacters[_characterIndex].imageURI,
+      hp: defaultCharacters[_characterIndex].hp,
+      maxHp: defaultCharacters[_characterIndex].maxHp,
+      attackDamage: defaultCharacters[_characterIndex].attackDamage
+});
+
+console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+// keep track of who owns the nft
+nftHolders[msg.sender] = newItemId;
+
+//fx from OpenZeppelin
+ _tokenIds.increment();
+
     }
 }
 
