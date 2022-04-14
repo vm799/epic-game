332e38fd61659dd5a3a978ae9a38624be381266a
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-14 14:14:34 +0100

    using tokenURI function to access NFT data

--- scripts/run.js
+++ scripts/run.js
@@ -18,6 +18,13 @@ const main = async () =>{
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 
+    let txn;
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
+    //tokenURI part of ERC721 function which returns all data att to NFT
 };
 
 const runMain = async ()=>{
