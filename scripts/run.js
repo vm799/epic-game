e7a4dac9d2fdb175f13e95e998599ea71287dda0
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-04-14 13:32:26 +0100

    updated characters

--- scripts/run.js
+++ scripts/run.js
@@ -7,7 +7,14 @@
 
 const main = async () =>{
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Sarah", "Bruce", "She-Ra"],
+        ["https://imgur.com/AEKcEOl.png",
+        "https://i.imgur.com/aDIUIUd.jpeg",
+        "https://i.imgur.com/3ByvYV2.jpeg"],
+        [100,200,300],  //HP values
+        [100,50,25]      //Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 
